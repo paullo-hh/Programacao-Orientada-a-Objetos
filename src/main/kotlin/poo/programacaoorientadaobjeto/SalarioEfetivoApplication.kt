5d240cc0e0666be7a100090bf45d57e55b0c03d1
Paulo H. Carvalho <contacthenry96@gmail.com>
2024-04-15 15:59:57 -0300

    criação do projeto

---
+++ src/main/kotlin/poo/programacaoorientadaobjeto/SalarioEfetivoApplication.kt
@@ -0,0 +1,11 @@
+package poo.programacaoorientadaobjeto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalarioEfetivoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SalarioEfetivoApplication>(*args)
+}
---
+++ src/test/kotlin/poo/programacaoorientadaobjeto/ProgramacaoOrientadaObjetoApplicationTests.kt
@@ -0,0 +1,13 @@
+package poo.programacaoorientadaobjeto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProgramacaoOrientadaObjetoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
